651751082b6911e6a4ad13ecfc4d9d29c60b5362
BrownHu <1104524352@qq.com>
2018-08-18 21:43:26 +0800

    等级颜色划分

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="game">
-        <div class="row" v-for="row in gameBox">
-            <div class="col" v-for="number in row">{{number}}</div>
+        <div class="row animated" v-for="row in gameBox">
+            <div class="col animated zoomIn" :class="'n-' + number" v-for="number in row">{{number}}</div>
         </div>
     </section>
 </template> 
@@ -13,9 +13,29 @@ import { Component, Prop, Vue, Emit } from "vue-property-decorator";
 export default class Game extends Vue {
     gameBox: any[] = [];
     score: number = 0;
+    bestScore: number = 0;
     size: number = 4;
     over: boolean = true;
     emptyBox: any[] = [];
+    direction: object[] = [
+        {
+            x: 0, // up
+            y: -1
+        },
+        {
+            x: 0, // down
+            y: 1
+        },
+        {
+            x: -1, // left
+            y: 0
+        },
+        {
+            x: 1, // right
+            y: 0
+        }
+    ];
+    firstLine: number[] = [];
     @Emit()
     newGame() {
         this.score = 0;
@@ -26,7 +46,6 @@ export default class Game extends Vue {
         document.addEventListener("keyup", this.keyDown);
     }
     keyDown(e: any) {
-        let arr = null;
         switch (e.keyCode) {
             case 38: //上
                 this.move(1);
@@ -70,14 +89,87 @@ export default class Game extends Vue {
         }
         return this.emptyBox;
     }
+    moveLeft(list: number[]) {
+        let _list: object[] = []; //当前行非空格子
+        let flg: boolean = false;
+        for (let i = 0; i < this.size; i++) {
+            if (list[i]) {
+                _list.push({
+                    x: i,
+                    merged: false,
+                    value: list[i]
+                });
+            }
+        }
+        interface numberType {
+            x: any;
+            merged: boolean;
+            value: number;
+        }
+        _list.forEach((item: numberType) => {
+            let farthest = this.farthestPosition(list, item);
+            let next: number = list[farthest - 1];
+            if (next && next === item.value && !_list[farthest - 1].merged) {
+                //合并
+                list[farthest - 1] = next * 2;
+                list[item.x] = undefined;
+                item = {
+                    x: farthest - 1,
+                    merged: true,
+                    value: next * 2
+                };
+                this.score += next * 2;
+            } else {
+                if (farthest != item.x) {
+                    list[farthest] = item.value;
+                    list[item.x] = undefined;
+                    item.x = farthest;
+                }
+            }
+        });
+        return list;
+    }
+    farthestPosition(list: number[], cell: {}) {
+        let farthest = cell.x;
+        while (farthest > 0 && !list[farthest - 1]) {
+            farthest = farthest - 1;
+        }
+        return farthest;
+    }
     move(i: number) {
-        let arr = this.rotate(Array.from(this.list), i).map((item, index) => {
+        let arr = this.rotate(this.gameBox, i).map(item => {
             return this.moveLeft(item);
         });
-        this.list = this.rotate(arr, this.size - i);
+        this.gameBox = this.rotate(arr, this.size - i);
         this.setLocalstorage();
-        if (!this.isAvailable()) {
+        if (this.getEmptyBox().length === 0) {
             this.over = true;
+        }
+    }
+    rotate(arr: any[], n: number) {
+        n = n % 4;
+        if (n === 0) return arr;
+        let tmp: any[] = Array.from(Array(this.size)).map(() =>
+            Array(this.size).fill(undefined)
+        );
+        for (let i = 0; i < this.size; i++) {
+            for (let j = 0; j < this.size; j++) {
+                tmp[this.size - 1 - i][j] = arr[j][i];
+            }
+        }
+        if (n > 1) tmp = this.rotate(tmp, n - 1);
+        return tmp;
+    }
+    setLocalstorage() {
+        let score = localStorage.getItem("bestScore");
+        if (score) {
+            if (this.score > +score) {
+                localStorage.setItem("bestScore", `${this.score}`);
+                this.bestScore = this.score;
+            }
+        } else {
+            localStorage.setItem("bestScore", `${this.score}`);
+            this.bestScore = this.score;
         }
     }
     mounted() {
@@ -89,17 +181,61 @@ export default class Game extends Vue {
 <style lang="less" scoped>
 .row {
     display: flex;
-    height: 100px;
-    width: 400px;
+    height: 125px;
+    width: 500px;
+    background: #bbada0;
+    align-items: center;
+    justify-content: space-around;
     .col {
         display: flex;
-        justify-content: center;
         align-items: center;
-        height: 100px;
-        width: 100px;
-        background: #333;
-        color: white;
+        justify-content: center;
         font-size: 24px;
+        border-radius: 3px;
+        height: 105px;
+        width: 105px;
+        color: #776e65;
+        background: #cdc1b4;
+        &.n-2 {
+            background: #eee4da;
+        }
+        &.n-4 {
+            background: #ede0c8;
+        }
+
+        &.n-8 {
+            background: #F2B179;
+        }
+        &.n-16 {
+            background: #F59563;
+        }
+        &.n-32 {
+            background: #F67C5F;
+        }
+        &.n-64 {
+            background: #F65E3B;
+        }
+        &.n-128 {
+            background: #EDCF72;
+        }
+        &.n-256 {
+            background: #EDCC61;
+        }
+        &.n-512 {
+            background: #EDC850;
+        }
+        &.n-1024 {
+            background: #EDC53F;
+        }
+        &.n-2048 {
+            background: #edc22e;
+        }
+        &.n-4096 {
+            background: #3c3a32;
+        }
+        &.n-8192 {
+            background: #3c3a32;
+        }
     }
 }
 </style>
